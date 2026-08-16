adcc2a29e8e849ccd418a47e4afac8513806c93a
viya-c <1032237642@qq.com>
2026-08-16 20:55:34 +0800

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -4,4 +4,10 @@
  *   把下面改成你的 Worker 地址，例如 'https://xingguang.workers.dev'
  *   前端所有 /api 请求与实时同步 WebSocket 都会指向该地址，实现家人之间实时同步。
  */
-window.__API_BASE = 'https://xingguang-banxuewu.1032237642.workers.dev';
+window.__GITHUB = {
+  owner: '你的GitHub用户名',          // 例如网址 github.com/小明/xingguang 里的「小明」
+  repo: 'xingguang',                // 仓库名，一般就是这个
+  branch: 'main',                   // 默认分支
+  dataPath: 'data/state.json',       // 不用改
+  token: 'github_pat_这里粘贴你刚复制的token'
+};
