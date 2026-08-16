bc24b8167300a3461d583c1152d002aad7ce73b8
viya-c <1032237642@qq.com>
2026-08-16 23:27:08 +0800

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,6 +1,6 @@
 window.__GITHUB = {
   owner: 'viya-c',
-  repo: 'viya-c.github.io',
+  repo: 'banxuewu',
   branch: 'main',
   dataPath: 'state.json',
   token: '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'
