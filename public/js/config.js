f1172980e38f4e3bbf200d92e206978b59fad31c
viya-c <1032237642@qq.com>
2026-08-23 22:29:47 +0800

    Delete public directory

--- public/js/config.js
+++
@@ -1,7 +0,0 @@
-window.__GITHUB = {
-  owner: 'viya-c',
-  repo: 'banxuewu',
-  branch: 'main',
-  dataPath: 'state.json',
-  token: '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'
-};
